8925e112490d49a64b7ab73351b88f2103a82c9e
Kulwant Bawa <44430853+kulwantablu@users.noreply.github.com>
2025-07-22 16:55:48 +0530

    Update students.js   get single student

--- routes/students.js
+++ routes/students.js
@@ -3,18 +3,15 @@ const router = express.Router();
 const auth = require("../middleware/authMiddleware");
 const {
   getStudents,
+  singleStudents,
   createStudents,
   updateStudents,
   deleteStudents,
 } = require("../controllers/studentController");
 
-console.log("getStudents:", typeof getStudents); // should be 'function'
-console.log("createStudents:", typeof createStudents);
-console.log("updateStudents:", typeof updateStudents);
-console.log("deleteStudents:", typeof deleteStudents);
-
 router.get("/", auth, getStudents);
 router.post("/", createStudents);
+router.get("/:id", singleStudents);
 router.put("/:id", updateStudents);
 router.delete("/:id", deleteStudents);
 
