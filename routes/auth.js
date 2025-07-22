c177890c3078088d8d5ee3a92a52d7aaf7d2292e
Kulwant Bawa <kulwantablu@gmail.com>
2025-07-22 14:40:45 +0530

    update changes login

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/authController");
+const { login, register } = require("../controllers/authController");
 
-router.post("/register", register);
 router.post("/login", login);
+router.post("/register", register);
 
 module.exports = router;
